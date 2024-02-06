481ec804a6396fb82a175c316abb64efd7a13e89
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-06 18:15:51 +1000

    correction

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,15 +5,15 @@
 
 int[] array = { 1, 3, 2, 4, 2, 3 };
 
-int n = 6 - 1;
+int n = array.Length - 1;
 
-int[] result = new int[6 / 2];
+int[] result = new int[array.Length / 2];
 
-for (int i = 0; i < n; i++)
+for (int i = 0; i < n - i; i++)
 {
-    result[i] = array[i] * array[n-1];
+    result[i] = array[i] * array[n-i];
 }
 for (int i = 0; i < result.Length; i++)
 {
-    System.Console.WriteLine(result[i] + " ");
+    System.Console.Write(result[i] + " ");
 }
